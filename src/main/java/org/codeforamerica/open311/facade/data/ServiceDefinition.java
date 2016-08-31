9f2f9b1384e02a48e4fe0b43736ce76ae4c1f416
milovanderlinden <milo@dogodigi.net>
2016-08-31 20:06:58 +0200

    GSON serialization

--- src/main/java/org/codeforamerica/open311/facade/data/ServiceDefinition.java
+++ src/main/java/org/codeforamerica/open311/facade/data/ServiceDefinition.java
@@ -3,6 +3,10 @@ package org.codeforamerica.open311.facade.data;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.google.gson.annotations.SerializedName;
+
+import org.codeforamerica.open311.internals.parsing.DataParser;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,54 +15,55 @@ import java.util.List;
  * Represents a <a
  * href="http://wiki.open311.org/GeoReport_v2#GET_Service_Definition">service
  * definition</a>.
- * 
+ *
  * @author Santiago Munín <santimunin@gmail.com>
- * 
  */
 public class ServiceDefinition implements Serializable, Parcelable {
 
-	private static final long serialVersionUID = -2002090161736619870L;
-	private String serviceCode;
-	private List<AttributeInfo> attributes;
+    private static final long serialVersionUID = -2002090161736619870L;
+    @SerializedName(DataParser.SERVICE_CODE_TAG)
+    private String serviceCode;
+    @SerializedName(DataParser.ATTRIBUTES_TAG)
+    private List<AttributeInfo> attributes;
 
-	public ServiceDefinition(String serviceCode, List<AttributeInfo> attributes) {
-		super();
-		this.serviceCode = serviceCode;
-		this.attributes = attributes;
-	}
+    public ServiceDefinition(String serviceCode, List<AttributeInfo> attributes) {
+        super();
+        this.serviceCode = serviceCode;
+        this.attributes = attributes;
+    }
 
-	public String getServiceCode() {
-		return serviceCode;
-	}
+    public String getServiceCode() {
+        return serviceCode;
+    }
 
-	public List<AttributeInfo> getAttributes() {
-		return attributes;
-	}
+    public List<AttributeInfo> getAttributes() {
+        return attributes;
+    }
 
-	@Override
-	public int describeContents() {
-		return 0;
-	}
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
-	@Override
-	public void writeToParcel(Parcel dest, int flags) {
-		dest.writeString(this.serviceCode);
-		dest.writeList(this.attributes);
-	}
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.serviceCode);
+        dest.writeList(this.attributes);
+    }
 
-	protected ServiceDefinition(Parcel in) {
-		this.serviceCode = in.readString();
-		this.attributes = new ArrayList<AttributeInfo>();
-		in.readList(this.attributes, List.class.getClassLoader());
-	}
+    protected ServiceDefinition(Parcel in) {
+        this.serviceCode = in.readString();
+        this.attributes = new ArrayList<AttributeInfo>();
+        in.readList(this.attributes, List.class.getClassLoader());
+    }
 
-	public static final Parcelable.Creator<ServiceDefinition> CREATOR = new Parcelable.Creator<ServiceDefinition>() {
-		public ServiceDefinition createFromParcel(Parcel source) {
-			return new ServiceDefinition(source);
-		}
+    public static final Parcelable.Creator<ServiceDefinition> CREATOR = new Parcelable.Creator<ServiceDefinition>() {
+        public ServiceDefinition createFromParcel(Parcel source) {
+            return new ServiceDefinition(source);
+        }
 
-		public ServiceDefinition[] newArray(int size) {
-			return new ServiceDefinition[size];
-		}
-	};
+        public ServiceDefinition[] newArray(int size) {
+            return new ServiceDefinition[size];
+        }
+    };
 }
