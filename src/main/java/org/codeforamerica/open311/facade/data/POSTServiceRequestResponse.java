c756a66800e78d4d7b716bf3ff5ff96c51df368b
milovanderlinden <milo@dogodigi.net>
2016-08-31 20:02:15 +0200

    GSON serialization

--- src/main/java/org/codeforamerica/open311/facade/data/POSTServiceRequestResponse.java
+++ src/main/java/org/codeforamerica/open311/facade/data/POSTServiceRequestResponse.java
@@ -1,42 +1,49 @@
 package org.codeforamerica.open311.facade.data;
 
+import com.google.gson.annotations.SerializedName;
+
+import org.codeforamerica.open311.internals.parsing.DataParser;
+
 /**
  * Wraps a POST service request response. <a
  * href="http://wiki.open311.org/GeoReport_v2#POST_Service_Request">More
  * info.</a>.
- * 
+ *
  * @author Santiago Munín <santimunin@gmail.com>
- * 
  */
 public class POSTServiceRequestResponse {
-	private String serviceRequestId;
-	private String token;
-	private String serviceNotice;
-	private String accountId;
+    @SerializedName(DataParser.SERVICE_REQUEST_ID_TAG)
+    private String serviceRequestId;
+    @SerializedName(DataParser.TOKEN_TAG)
+    private String token;
+    @SerializedName(DataParser.SERVICE_NOTICE_TAG)
+    private String serviceNotice;
+    @SerializedName(DataParser.ACCOUNT_ID_TAG)
+    private String accountId;
 
-	public POSTServiceRequestResponse(String serviceRequestId, String token,
-			String serviceNotice, String accountId) {
-		super();
-		this.serviceRequestId = serviceRequestId;
-		this.token = token;
-		this.serviceNotice = serviceNotice;
-		this.accountId = accountId;
-	}
+    public POSTServiceRequestResponse(String serviceRequestId, String token,
+                                      String serviceNotice, String accountId) {
+        super();
+        this.serviceRequestId = serviceRequestId;
+        this.token = token;
+        this.serviceNotice = serviceNotice;
+        this.accountId = accountId;
+    }
 
-	public String getServiceRequestId() {
-		return serviceRequestId;
-	}
+    public String getServiceRequestId() {
+        return serviceRequestId;
+    }
 
-	public String getToken() {
-		return token;
-	}
+    public String getToken() {
+        return token;
+    }
 
-	public String getServiceNotice() {
-		return serviceNotice;
-	}
+    public String getServiceNotice() {
+        return serviceNotice;
+    }
 
-	public String getAccountId() {
-		return accountId;
-	}
+    public String getAccountId() {
+        return accountId;
+    }
 
 }
